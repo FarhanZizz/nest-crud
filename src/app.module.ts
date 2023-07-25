f0006cb90b366afb149f5e07fb23e741af25fd3f
FarhanZizz <iamfarhanaziz@gmail.com>
2023-07-25 21:31:00 +0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppResolver } from './app.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { BookModule } from './book/book.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+    BookModule,
+  ],
+  controllers: [],
+  providers: [AppResolver],
+})
+export class AppModule {}
